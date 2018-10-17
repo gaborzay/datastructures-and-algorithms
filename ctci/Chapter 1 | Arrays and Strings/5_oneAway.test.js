101eec31d351aa1355aee65e1e1c8c02d7acccc7
Gabor Zay <GaborZay@gmail.com>
2018-10-17 11:22:11 -0700

    Solved 'One Way'

---
+++ ctci/Chapter 1 | Arrays and Strings/5_oneAway.test.js
@@ -0,0 +1,79 @@
+/**
+ * One Away
+ * There are three types of edits that can be performed on strings: insert a
+ * character, remove a character, or replace a character. Given two strings,
+ * write a function to check if they are on edit (or zero edits) away.
+ * */
+
+const strFuncs = require('./String/String');
+
+// Removing Character
+test('returns true when removing first character', () => {
+  const str1 = "pale";
+  const str2 = "ale";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+
+test('returns true when removing character from middle part', () => {
+  const str1 = "pale";
+  const str2 = "ple";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+
+test('returns true when removing last character', () => {
+  const str1 = "pale";
+  const str2 = "pal";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+// Inserting Character
+test('returns true when inserting first character', () => {
+  const str1 = "ales";
+  const str2 = "pales";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+
+test('returns true when inserting middle character', () => {
+  const str1 = "ples";
+  const str2 = "pales";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+
+test('returns true when inserting last character', () => {
+  const str1 = "pales";
+  const str2 = "pale";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+// Replacing Character
+test('returns true when replacing first character', () => {
+  const str1 = "pale";
+  const str2 = "bale";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+
+test('returns true when replacing middle character', () => {
+  const str1 = "pale";
+  const str2 = "pare";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+
+test('returns true when replacing last character', () => {
+  const str1 = "pale";
+  const str2 = "pali";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeTruthy();
+});
+// Failing Case
+test('returns false more than one away', () => {
+  const str1 = "pale";
+  const str2 = "bake";
+  const result = strFuncs.oneAway(str1, str2);
+  expect(result).toBeFalsy();
+});
--- ctci/Chapter 1 | Arrays and Strings/String/String.js
+++ ctci/Chapter 1 | Arrays and Strings/String/String.js
@@ -5,7 +5,7 @@ module.exports = {
   // Space complexity: O(0)
   isUnique: (str) => {
     // Remove all white space
-    str = str.toLowerCase().replace(/\s/g, '');
+    str = str.replace(/\s/g, '').toLowerCase();
     const n = str.length;
     for (let i = 0; i < n; i++) {
       for (let j = i + 1; j < n; j++) {
@@ -19,7 +19,7 @@ module.exports = {
   isUniqueHash: (str) => {
     let hashTable = {};
     // Remove all white space
-    str = str.toLowerCase().replace(/\s/g, '');
+    str = str.replace(/\s/g, '').toLowerCase();
     const n = str.length;
     for (let i = 0; i < n; i++) {
       if (hashTable[str[i]]) return false;
@@ -30,9 +30,9 @@ module.exports = {
   // Time complexity: O(n*log(n)) -> from sort
   // Space complexity: O(log(n)) -> from sort
   isPermutation: (str1, str2) => {
-    // Remove all white space and sort string
-    str1 = str1.toLowerCase().replace(/\s/g, '').split('').sort().join('');
-    str2 = str2.toLowerCase().replace(/\s/g, '').split('').sort().join('');
+    // Remove all white space
+    str1 = str1.replace(/\s/g, '').toLowerCase().split('').sort().join('');
+    str2 = str2.replace(/\s/g, '').toLowerCase().split('').sort().join('');
     if (str1.length !== str2.length) return false;
     const n = str1.length;
     for (let i = 0; i < n; i++) {
@@ -55,7 +55,8 @@ module.exports = {
   // Time complexity: O(n)
   // Space complexity: O(256) -> 2 bytes per character in JS
   palindromePermutation: (str) => {
-    str = str.toLowerCase().replace(/\s/g, '').split('').sort();
+    // Remove all white space
+    str = str.replace(/\s/g, '').toLowerCase().split('').sort();
     const n = str.length;
     let hashTable = {};
     let oddChars = 0;
@@ -63,9 +64,64 @@ module.exports = {
       if (!hashTable[str[i]]) hashTable[str[i]] = 1;
       else hashTable[str[i]] += 1;
     }
+    // Count number of odd characters
     for (let char in hashTable) {
       if (hashTable.hasOwnProperty(char) && hashTable[char] % 2 !== 0) oddChars++;
     }
     return oddChars < 2;
+  },
+  // Time complexity: O(n)
+  // Space complexity: O(26)
+  oneAway: (str1, str2) => {
+    // Remove all white space
+    str1 = str1.replace(/\s/g, '').toLowerCase();
+    str2 = str2.replace(/\s/g, '').toLowerCase();
+    // Helper Functions
+    const getAlphabet = () => {
+      const alphabet = [];
+      for (let i = 97; i < 123; i++) {
+        alphabet.push(String.fromCharCode(i));
+      }
+      return alphabet;
+    };
+    const removeCharacter = (str, n, index) => {
+      const front = str.slice(0, index);
+      const end = str.slice(index + 1, n);
+      return front.concat(end);
+    };
+    const insertCharacter = (str, n, index, char) => {
+      const front = str.slice(0, index);
+      const end = str.slice(index + 1, n);
+      return front.concat(char, end);
+    };
+    const replaceCharacter = (str, n, index, char) => {
+      const front = str.slice(0, index);
+      const end = str.slice(index, n);
+      return front.concat(char, end);
+    };
+    // Constants
+    const n = str1.length;
+    const alphabet = getAlphabet();
+    const alphaLen = alphabet.length;
+    // Check if removing a character makes strings equal
+    for (let i = 0; i < n; i++) {
+      let strRemoved = removeCharacter(str1, n, i);
+      if (strRemoved === str2) return true;
+    }
+    // Check if inserting a character makes strings equal
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < alphaLen; j++) {
+        let strInserted = insertCharacter(str1, n, i, alphabet[j]);
+        if (strInserted === str2) return true;
+      }
+    }
+    // Check if replacing a character makes strings equal
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < alphaLen; j++) {
+        let strReplaced = replaceCharacter(str1, n, i, alphabet[j]);
+        if (strReplaced === str2) return true;
+      }
+    }
+    return false;
   },
 };
